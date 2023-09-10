19b27d0d66484f20df174433a9992da41f385a59
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-10 11:58:59 -0400

    update logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,10 @@
-import { Bug, Search } from "./Icons";
+import { Error, Search } from "./Icons";
 
 const Nav = () => {
   return (
     <div className="w-full bg-slate-700 h-[8vh] flex items-center justify-between px-4">
       <div className="flex items-center">
-        <Bug />
+        <Error className="w-8 h-8 text-red-600 font-bold mr-2" />
         <h1 className="font-bold text-white">Github Issues Viewer</h1>
       </div>
       <div className="flex items-center bg-slate-600 h-8 border border-slate-500 rounded-sm">
