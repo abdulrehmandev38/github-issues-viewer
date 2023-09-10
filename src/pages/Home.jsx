213022e7c86646e8f272a084894c27fd1f5c6d0a
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-10 10:38:14 -0400

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@apollo/client";
+import Paper from "../components/Paper";
+import ReposList from "../components/ReposList";
+import { SEARCH_REPOSITORIES } from "../api/queries";
+
+const Home = () => {
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+    variables: { repo: "react" },
+  });
+  return (
+    <Paper>
+      <ReposList loading={loading} repositories={data?.search?.edges} />
+    </Paper>
+  );
+};
+export default Home;
