600d867969fa66474b85840dafe693fa92729755
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-11 13:51:03 -0400

    implement get issues query

--- src/pages/Repo.jsx
+++ src/pages/Repo.jsx
@@ -2,9 +2,14 @@ import IssuesList from "../components/IssuesList";
 import RepoDetails from "../components/RepoDetails";
 import { useParams } from "react-router-dom";
 import Paper from "../components/Paper";
+import { useQuery } from "@apollo/client";
+import { GET_ISSUES } from "../api/queries";
 
 const Repo = () => {
   const { repo } = useParams();
+  const { laoding, error, data } = useQuery(GET_ISSUES, {
+    variables: { owner: repo.split("-")[0], name: repo.split("-")[1] },
+  });
   return (
     <Paper>
       <RepoDetails
@@ -12,7 +17,7 @@ const Repo = () => {
         stars={123}
         description="lorem ipsum dollar nothing to do within"
       />
-      <IssuesList />
+      <IssuesList laoding={laoding} issues={data?.repository?.issues?.nodes} />
     </Paper>
   );
 };
