0dca30ecc24157455f358119d86712fd6b5f331f
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-11 16:51:38 -0400

    implement repo details with loading

--- src/components/RepoDetails.jsx
+++ src/components/RepoDetails.jsx
@@ -1,18 +1,51 @@
+import { useParams } from "react-router-dom";
 import { Star } from "./Icons";
+import { useQuery } from "@apollo/client";
+import { GET_REPO_DETAILS } from "../api/queries";
 
-const RepoDetails = ({ name, description, stars }) => {
+const RepoDetailsLoading = () => {
   return (
     <div className="bg-slate-700 w-full px-36 py-6">
       <div>
-        <div>
-          <p className="group-hover:underline text-4xl text-blue-600 cursor-pointer text-[20px] font-bold">
-            {name}
-          </p>
-          <p className="text-gray-400 text-lg">{description}</p>
+        <div className="flex flex-col gap-4">
+          <div className="bg-gray-300 animate-pulse w-48 rounded-md h-8"></div>
+          <div className="bg-gray-300 animate-pulse w-92 rounded-md h-8 "></div>
+          <div className="bg-gray-300 animate-pulse w-80 rounded-md h-8"></div>
         </div>
         <div className="flex items-center mt-4">
-          <p className="text-white font-bold text-[20px] mr-2">{stars}</p>
+          <div className="bg-gray-300 animate-pulse w-24 h-8 rounded-sm"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RepoDetails = () => {
+  const { repo } = useParams();
+  const [owner, name] = repo.split("-");
+  const { loading, data, error } = useQuery(GET_REPO_DETAILS, {
+    variables: {
+      owner,
+      name,
+    },
+  });
+  if (loading) return <RepoDetailsLoading />;
+  return (
+    <div className="bg-slate-700 w-full px-36 py-6">
+      <div>
+        <div className="flex flex-col gap-4">
+          <p className="group-hover:underline text-4xl text-blue-600 text-[20px] font-bold">
+            {owner + "/" + name}
+          </p>
+          <p className="text-gray-100 text-lg">
+            {data?.repository?.description}
+          </p>
+        </div>
+        <div className="flex items-center mt-4">
           <Star />
+          <p className="text-white font-bold text-[15px] ml-1">
+            {data?.repository?.stargazerCount}
+          </p>
         </div>
       </div>
     </div>
