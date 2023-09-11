040a407b78db075c525d593b79b440a9966d88d3
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-11 16:50:45 -0400

    move issue state buttons to repo page comp

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,47 +1,34 @@
 import { Error } from "./Icons";
-import IssueItem from "./IssueItem";
-import { RepoItemLoading } from "./RepoItem";
+import IssueItem, { IssueItemLoading } from "./IssueItem";
 
-const IssuesList = ({ laoding, issues }) => {
+const IssuesList = ({ loading, issues }) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-start justify-start gap-4">
-        <div className="flex items-center justify-center hover:bg-slate-800 cursor-pointer py-3 px-2 border-b-2 border-green-500">
-          <Error className="w-7 h-7 text-green-500" />
-          <p className="ml-2 font-bold text-white">Open</p>
-        </div>
-        <div className="flex items-center justify-center hover:bg-slate-800 cursor-pointer py-3 px-2 border-b-2 border-red-500">
-          <Error className="w-7 h-7 text-red-500" />
-          <p className="ml-2 font-bold text-white">Closed</p>
-        </div>
-      </div>
-      <div className="flex h-[65vh] flex-col items-center gap-4 px-8 hover:px-4 pt-8 overflow-hidden hover:overflow-y-scroll">
-        {laoding
-          ? Array(8)
-              .fill(0)
-              .map((i) => {
-                return <RepoItemLoading />;
-              })
-          : issues?.map((issue) => {
-              return (
-                <IssueItem
-                  state={issue.state}
-                  title={
-                    issue.title.length < 40
-                      ? issue.title
-                      : issue.title.slice(0, 40) + "..."
-                  }
-                  number={issue.number}
-                  description={
-                    issue.body.length < 70
-                      ? issue.body
-                      : issue.body.slice(0, 70) + "..."
-                  }
-                  comments={issue.comments.totalCount}
-                />
-              );
-            })}
-      </div>
+    <div className="flex h-[65vh] flex-col items-center gap-4 px-8 hover:px-4 pt-8 overflow-hidden hover:overflow-y-scroll">
+      {loading
+        ? Array(8)
+            .fill(0)
+            .map((i) => {
+              return <IssueItemLoading />;
+            })
+        : issues?.map((issue) => {
+            return (
+              <IssueItem
+                state={issue.state}
+                title={
+                  issue.title.length < 40
+                    ? issue.title
+                    : issue.title.slice(0, 40) + "..."
+                }
+                number={issue.number}
+                description={
+                  issue.body.length < 70
+                    ? issue.body
+                    : issue.body.slice(0, 70) + "..."
+                }
+                comments={issue.comments.totalCount}
+              />
+            );
+          })}
     </div>
   );
 };
--- src/pages/Repo.jsx
+++ src/pages/Repo.jsx
@@ -4,10 +4,11 @@ import { useParams } from "react-router-dom";
 import Paper from "../components/Paper";
 import { useQuery } from "@apollo/client";
 import { GET_ISSUES } from "../api/queries";
+import { Error } from "../components/Icons";
 
 const Repo = () => {
   const { repo } = useParams();
-  const { laoding, error, data } = useQuery(GET_ISSUES, {
+  const { loading, error, data } = useQuery(GET_ISSUES, {
     variables: { owner: repo.split("-")[0], name: repo.split("-")[1] },
   });
   return (
@@ -17,7 +18,17 @@ const Repo = () => {
         stars={123}
         description="lorem ipsum dollar nothing to do within"
       />
-      <IssuesList laoding={laoding} issues={data?.repository?.issues?.nodes} />
+      <div className="flex items-start justify-start gap-4 w-full px-24 mb-2">
+        <div className="flex items-center justify-center hover:bg-slate-800 cursor-pointer py-3 px-2 border-b-2 border-green-500">
+          <Error className="w-7 h-7 text-green-500" />
+          <p className="ml-2 font-bold text-white">Open</p>
+        </div>
+        <div className="flex items-center justify-center hover:bg-slate-800 cursor-pointer py-3 px-2 border-b-2 border-red-500">
+          <Error className="w-7 h-7 text-red-500" />
+          <p className="ml-2 font-bold text-white">Closed</p>
+        </div>
+      </div>
+      <IssuesList loading={loading} issues={data?.repository?.issues?.nodes} />
     </Paper>
   );
 };
