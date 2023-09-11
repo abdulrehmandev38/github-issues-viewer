bde8a841374a1096ebcd172e4a5e0e4126920c25
abdulrehmandev388 <salahsaadaoui8@gmail.com>
2023-09-11 13:50:37 -0400

    add loading

--- src/components/ReposList.jsx
+++ src/components/ReposList.jsx
@@ -1,21 +1,27 @@
-import RepoItem from "./RepoItem";
+import RepoItem, { RepoItemLoading } from "./RepoItem";
 
 const ReposList = ({ repositories, loading }) => {
-  if (loading) return <p>WHoop</p>;
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-white font-bold text-2xl my-8">
         Find a Repositorie !
       </h1>
-      {repositories?.map((repo) => {
-        return (
-          <RepoItem
-            name={`${repo.node.owner.login}/${repo.node.name}`}
-            stars={repo.node.stargazerCount}
-            description={repo.node.description.slice(0, 50) + "..."}
-          />
-        );
-      })}
+      {loading
+        ? Array(8)
+            .fill(0)
+            .map((i) => {
+              return <RepoItemLoading />;
+            })
+        : repositories?.map((repo) => {
+            return (
+              <RepoItem
+                id={repo.node.id}
+                name={`${repo.node.owner.login}/${repo.node.name}`}
+                stars={repo.node.stargazerCount}
+                description={repo.node.description.slice(0, 50) + "..."}
+              />
+            );
+          })}
     </div>
   );
 };
